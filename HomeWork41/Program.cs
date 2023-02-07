8533ddb7684e2b54a70c50de40cac9cbf9188d02
Ekaterina <katya_usova@bk.ru>
2023-02-07 18:47:10 +0300

    HomeWork41

---
+++ HomeWork41/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ekaterina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Введите число M");
+            int M = System.Convert.ToInt32(System.Console.ReadLine());
+            int pr = 0;
+            for (int i = 0; i < M; i++)
+            {
+                System.Console.WriteLine("Введите число: ");
+                int a = System.Convert.ToInt32(System.Console.ReadLine());
+                if (a > 0) pr++;
+            }
+            System.Console.WriteLine("\nПоложительных: {0}", pr);
+            System.Console.ReadKey();
+        }
+    }
+}
